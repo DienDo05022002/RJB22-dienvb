872a3ba7472de47e4dea8dca4e336469e0a9bea3
dien <diendo.05022002@gmail.com>
2022-07-08 21:07:40 +0700

    init code

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+import Menu from './CP/components/menu';
+import Bai3 from './CP/components/Bai3'
+import Bai2 from './CP/components/B2';
+
+ 
+
+function App() {
+  return (
+    <>
+    <Menu></Menu>
+    <Bai2></Bai2>
+    <Bai3></Bai3>
+
+
+      {/* <JSX></JSX> */}
+      {/* <ES6></ES6> */}
+    </>
+  );
+}
+
+export default App;
+
